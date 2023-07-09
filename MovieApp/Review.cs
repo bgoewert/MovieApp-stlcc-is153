c083f0c6191d79f7f62eb869b62f060bb384e4ab
Brennan Goewert <brennan@goewert.me>
2023-07-09 15:35:40 -0700

    Add Rating class

---
+++ MovieApp/Review.cs
@@ -0,0 +1,10 @@
+﻿
+namespace MovieApp
+{
+    internal class Review
+    {
+        public string Username { get; set; }
+        public int Rating { get; set; }
+        public string Comment { get; set; }
+    }
+}
